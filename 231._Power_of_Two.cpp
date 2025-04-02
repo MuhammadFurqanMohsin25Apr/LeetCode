5bebb95ea2a46fd03eae23833284cd0a40db96eb
Muhammad Furqan Mohsin <furqanmohammed788@gmail.com>
2025-04-02 10:17:24 -0700

    231._Power_of_Two.cpp

--- 231._Power_of_Two.cpp
+++ 231._Power_of_Two.cpp
@@ -2,7 +2,7 @@ class Solution {
     public:
         bool isPowerOfTwo(int n) {
             int m=0;
-            while(m!=32){
+            while(m!=30){
                 if(pow(2,m)==n){
                     return true;
                 }
